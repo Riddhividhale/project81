d1c7a97b70e373483276f561d0ed3abdcfe5b416
Riddhividhale <87593958+Riddhividhale@users.noreply.github.com>
2021-12-19 15:49:14 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,36 @@
+canvas = document.getElementById("myCanvas");
+ ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle="blue";
+ctx.lineWidth=2;
+ctx.arc(500,200,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="yellow";
+ctx.lineWidth=4;
+ctx.arc(400,200,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="black";
+ctx.lineWidth=6;
+ctx.arc(450,250,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="green";
+ctx.lineWidth=7;
+ctx.arc(350,240,40,0,2*Math.PI);
+ctx.stroke();
+ 
+ctx.beginPath();
+ctx.strokeStyle="red";
+ctx.lineWidth=8;
+ctx.arc(300,200,40,0,2*Math.PI);
+ctx.stroke();
+ 
+
+ 
+ 
